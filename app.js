da261f69da3f92e6bb9ed94b3f7ecf99b361b30d
firdavsxon@gmail.com <firdavsxon@gmail.com>
2024-04-20 10:07:05 +0500

    4-oy 1-vazifa

---
+++ app.js
@@ -0,0 +1,14 @@
+let ism = prompt("Ismingizni kiriting:")
+
+let boshHarf = ism.charAt().toUpperCase()
+let davomi = ism.slice(1).toLowerCase()
+let result = boshHarf + davomi
+
+alert(`salom, ${result}`)
+
+
+
+//charAt()
+//slice()
+//toUpperCase()
+//toLowerCase()
